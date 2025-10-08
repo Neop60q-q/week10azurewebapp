e73652133dfa15a23daddf9c59e2fbfc2abf62ad
Neop60q-q <thdtaka29@gmail.com>
2025-10-08 11:47:10 +1100

    change page text, see whats up

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+            WHAT THE HELL IS WEB APP DEV!?!?!?
           </li>
         </ol>
 
